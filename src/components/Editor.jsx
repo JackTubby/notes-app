ac7bd27f35be73159f3dd875b0abee891f300862
JackTubby <jack200034@hotmail.com>
2022-10-20 15:58:00 +0100

    Add correct editor code

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,27 +1,30 @@
 import React from "react"
+import ReactMde from "react-mde"
+import Showdown from "showdown"
 
-export default function Sidebar(props) {
-    const noteElements = props.notes.map((note, index) => (
-        <div key={note.id}>
-            <div
-                
-                className={`title ${
-                    note.id === props.currentNote.id ? "selected-note" : ""
-                }`}
-                onClick={() => props.setCurrentNoteId(note.id)}
-            >
-                <h4 className="text-snippet">Note {index + 1}</h4>
-            </div>
-        </div>
-    ))
+export default function Editor({ currentNote, updateNote }) {
+    const [selectedTab, setSelectedTab] = React.useState("write")
+
+    const converter = new Showdown.Converter({
+        tables: true,
+        simplifiedAutoLink: true,
+        strikethrough: true,
+        tasklists: true,
+    })  
 
     return (
-        <section className="pane sidebar">
-            <div className="sidebar--header">
-                <h3>Notes</h3>
-                <button className="new-note" onClick={props.newNote}>+</button>
-            </div>
-            {noteElements}
+        <section className="pane editor">
+            <ReactMde
+                value={currentNote.body}
+                onChange={updateNote}
+                selectedTab={selectedTab}
+                onTabChange={setSelectedTab}
+                generateMarkdownPreview={(markdown) =>
+                    Promise.resolve(converter.makeHtml(markdown))
+                }
+                minEditorHeight={80}
+                heightUnits="vh"
+            />
         </section>
     )
 }
